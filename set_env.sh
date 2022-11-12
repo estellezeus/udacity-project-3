9b4d419635801a73c5ca8d6603e68ead6733af6a
Estelle Z <zeukengestelle@gmail.com>
2022-11-12 05:42:33 +0100

    working in local containers

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagram_db_3
+export POSTGRES_PASSWORD=udagram_db_3
+export POSTGRES_HOST=projet-3-udagram-db.cm9hxo93dpy2.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram_db_3
+export AWS_BUCKET=udagram-3-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
